bdcd51c9247c336ccf206976f0e321eafaee20a6
Cristian Salvo <crisalvo@gmail.com>
2018-10-02 16:58:17 -0300

    Proyecto esqueleto

---
+++ src/app/services/peliculas.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Jsonp } from '@angular/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculasService {
+
+  private apikey = '';
+  private urlMoviedb = 'https://api.themoviedb.org/3';
+
+  constructor( private jsonp: Jsonp ) { console.log('conecta3'); }
+
+  getPopulares() {
+
+    const url = `${ this.urlMoviedb }/discover/movie?sort_by=popularity.desc&api_key=${ this.apikey }&language=es&callback=JSONP_CALLBACK`;
+
+    return this.jsonp.get( url )
+    .pipe(map(
+        res => res.json()
+      ));
+  }
+
+  buscarPelicula( texto: string ) {
+
+    const url = `${ this.urlMoviedb }/search/movie?query=${ texto }&sort_by=popularity.desc&api_key=${ this.apikey }&language=es&callback=JSONP_CALLBACK`;
+
+    return this.jsonp.get( url )
+      .pipe(map(
+        res => res.json()
+      ));
+  }
+
+}
